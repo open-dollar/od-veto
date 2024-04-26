7652f27e041bb8bcb21c3f5101c04a1ca149cf7e
Hunter King <hking6776@gmail.com>
2024-04-26 11:34:10 -0700

    init yarn

--- src/contracts/Veto.sol
+++ src/contracts/Veto.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
+import {IGovernor} from '@openzeppelin/governance/IGovernor.sol';
+
 contract Veto {}
--- test/unit/Veto.t.sol
+++ test/unit/Veto.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Veto} from "../src/Veto.sol";
+import {Test, console} from 'forge-std/Test.sol';
+import {Veto} from '@contracts/Veto.sol';
 
 contract Base is Test {}
