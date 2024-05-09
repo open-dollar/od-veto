e3e584526fed09d05e23db13a27159d3aea9b62d
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-08 20:36:22 -0700

    Add deployed

--- script/Registry.s.sol
+++ script/Registry.s.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.20;
 address constant MAINNET_OD_GOVERNOR = 0xf704735CE81165261156b41D33AB18a08803B86F;
 string constant MAINNET_OD_VETO_DELEGATE_PLEDGE =
   'I agree to use this contract for the express purpose of the protection of the Open Dollar protocol from sophisticated governance attacks, malicious actors, and proposals that would violate the terms or spirit of the Open Dollar protocol.';
-// address constant MAINNET_VETO = 0x;
+address constant MAINNET_VETO = 0x21109F493e03bA4FBe6095eB2Fb28af4d6B2C6b9;
 // --- Anvil Testnet ---
 
 // Token
